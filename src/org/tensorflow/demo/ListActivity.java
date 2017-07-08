9f4bdacbd97a36a4c45ebd2822409e8850fe72a3
pranavsh <pranavsh@qti.qualcomm.com>
2017-07-08 03:30:26 -0700

    Activitiaieieie Pt 2

---
+++ src/org/tensorflow/demo/ListActivity.java
@@ -0,0 +1,15 @@
+package org.tensorflow.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.snpeflow.demo.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
